c092d75ef34fda92f2e994a39b2738590b63ca99
Andrew Street <andrew_street@live.com>
2018-02-19 15:37:41 -0700

    update

--- ruby_golf.rb
+++ ruby_golf.rb
@@ -1,9 +1,68 @@
 #sum an array
-@a = [1, 2, 3, 4, 5]
-@s = 0
+# hole 1
+# @a = [1, 2, 3, 4, 5]
+# @s = 0
 
-def sum
-    @s = @a.reduce(0) { |x, y| x + y}
-end
-puts sum
+# def sum
+#     @s = @a.reduce(0) { |x, y| x + y}
+# end
+# puts sum
 #16
+# =====================================
+# hole 2
+# Takes a user input the computer randomly chooses 
+# and the winner is output to the screen.
+
+# def rps
+#   puts "Play rock paper scissors"
+#   puts "What's your choice R/P/S"
+#   imp = gets.strip
+#   c = ['r', 'p', 's']
+#   cc = c.sample
+#   if (imp == cc)
+#     puts "Tie"
+#   elsif (imp == 'r' && cc == 's')
+#     puts "You win"
+#   elsif (imp == 's' && cc == 'p')
+#   puts "You win"
+#   elsif (imp == 'p' && cc == 'r')
+#     puts "you win"
+#   else
+#     puts "You lose"
+#     exit
+#   end
+# end
+# rps
+# 135
+# ========================================
+# hole 3
+# For numbers 1-100 print “FIZZ” if the number is divisible by 3, “BUZZ” 
+# if the number is divisible by 5, “FIZZBUZZ” if the number is divisible by 
+# both 3 and 5 otherwise print the number.
+
+# def fb
+#   puts "Enter a number between 1 and 100"
+#   num = gets.to_i
+#   if (num%3 == 0 && num%5 == 0)
+#   puts "FIZZBUZZ"
+#   elsif (num%3 == 0)
+#     puts "Fizz"
+#   elsif (num%5 == 0)
+#     puts "Buzz"
+#   else
+#     puts "Too bad"
+#     exit
+#   end
+# end
+# fb
+# 77
+# ==========================================
+
+# hole 4
+# The method takes in a number and a max value then finds all multiples 
+# of the number up to the max value.
+
+def h4
+
+
+end
